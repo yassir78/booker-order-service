8d9b673bc957657fe962f8029b6812d5c0589d6c
yassir78 <yassir.acaf@gmail.com>
2022-01-29 14:01:34 +0100

    yassir:changes

--- src/main/java/com/fstg/bookerorderservice/application/dto/CustomOrderUpdateDto.java
+++ src/main/java/com/fstg/bookerorderservice/application/dto/CustomOrderUpdateDto.java
@@ -2,10 +2,7 @@ package com.fstg.bookerorderservice.application.dto;
 
 import lombok.Data;
 
-import java.math.BigDecimal;
-
 @Data
 public class CustomOrderUpdateDto {
     CustomerOrderDto customerOrderDto;
-
 }
